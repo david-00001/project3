46e967bf3429ccd8220484dad043630435e6bda8
david-00001 <rodgers07@gmail.com>
2023-06-03 19:09:53 -0600

    created state_info file

---
+++ state_info.js
@@ -0,0 +1,20 @@
+// URL to sample data
+const url = "https://api.covidtracking.com/v1/states/daily.json"
+
+// Fetch JSON data
+d3.json(url).then(function(data) {
+
+  // Retrieve names array for dropdown
+  var states = data.map(obj => obj.state);
+
+  // Populate the dropdown menu
+  var dropdown = d3.select("#selState");
+  states.forEach(function(state) {
+    dropdown.append("option").text(state).property("value", state);
+  });
+
+
+  console.log(data);
+
+
+});
