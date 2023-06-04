38aadec38fb7df68da649fae8003ca93e1c0db28
david-00001 <rodgers07@gmail.com>
2023-06-04 15:41:35 -0600

    got map working with dots

--- stateInfo.js
+++ stateInfo.js
@@ -62,9 +62,42 @@ const stateDictionary = {
 };
 
 
-// Fetch JSON data
-d3.json(url).then(function(data) {
+let data;
 
+// Function to handle changes in the dropdown selection
+function optionChanged(selectedState) {
+
+  // Filter data for selected state
+  var stateData = data.filter(function(obj) {
+    return obj.state === selectedState;
+  });
+
+  // Sort the stateData array based on the date in ascending order
+  stateData.sort(function(a, b) {
+    return a.date - b.date;
+  });
+
+  // Collect state info
+  var stateCount = stateData.length;   // Count the number of occurrences of the selected state
+  var firstDate = stateData[0].date;  // Get the first date from the selected state's data
+  var lastDate = stateData[stateData.length - 1].date;  // Get the first date from the selected state's data
+  var fullName = stateDictionary[selectedState];
+      
+  // Display state information
+  var stateInfo = d3.select("#stateInfo");
+  stateInfo.html("");
+  stateInfo.append("p").text(`Records for ${fullName}: ${stateCount}`);
+  stateInfo.append("p").text(`First Date: ${firstDate}`);
+  stateInfo.append("p").text(`Last Date: ${lastDate}`);
+}
+
+
+
+
+// Fetch JSON data
+d3.json(url).then(function(jsonData) {
+
+  data = jsonData
 
 
 
@@ -83,7 +116,7 @@ d3.json(url).then(function(data) {
   // Populate the dropdown menu
   var dropdown = d3.select("#selState");
   states.forEach(function(state) {
-    var fullName = stateDictionary[state] || state;
+    var fullName = stateDictionary[state];
     dropdown.append("option").text(`${state} - ${fullName}`).property("value", state);
   });
 
@@ -94,45 +127,15 @@ d3.json(url).then(function(data) {
 
 
 
-  // Function to handle changes in the dropdown selection
-  function optionChanged(selectedState) {
-
-    // Filter data for selected state
-    var stateData = data.filter(function(obj) {
-      return obj.state === selectedState;
-    });
-
-    // Sort the stateData array based on the date in ascending order
-    stateData.sort(function(a, b) {
-      return a.date - b.date;
-    });
- 
-    // Collect state info
-    var stateCount = stateData.length;   // Count the number of occurrences of the selected state
-    var firstDate = stateData[0].date;  // Get the first date from the selected state's data
-    var lastDate = stateData[stateData.length - 1].date;  // Get the first date from the selected state's data
-    var fullName = stateDictionary[selectedState];
-        
-    // Display state information
-    var stateInfo = d3.select("#stateInfo");
-    stateInfo.html("");
-    stateInfo.append("p").text(`Records for ${fullName}: ${stateCount}`);
-    stateInfo.append("p").text(`First Date: ${firstDate}`);
-    stateInfo.append("p").text(`Last Date: ${lastDate}`);
-  }
 
 
 
 
-
-
-
-
-  // Activate function when dropdown changes
-  dropdown.on("change", function() {
-    var selectedState = dropdown.property("value");
-    optionChanged(selectedState);
-  });
+  // // Activate function when dropdown changes
+  // dropdown.on("change", function() {
+  //   var selectedState = dropdown.property("value");
+  //   optionChanged(selectedState);
+  // });
    
 
 
